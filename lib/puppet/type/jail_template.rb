8ed297a8fffbf0e1f889c770fbbfe0fc042e139b
Igor Galić <igalic@brainsware.at>
2017-11-27 10:04:31 +0100

    swap newproperty & newparam
    every time i create a new type, i confuse what's supposed to be
    newparam, vs newproperty.
    
    I think the namevar is what's to blame: It's the first newparam() and
    only part that is a parameter, but also discoverable, so it's very
    confusing, especially since you start from here.

--- lib/puppet/type/jail_template.rb
+++ lib/puppet/type/jail_template.rb
@@ -8,15 +8,15 @@ Puppet::Type.newtype(:jail_template) do
     desc 'The name of the template'
   end
 
-  newparam(:release) do
+  newproperty(:release) do
     desc 'The release this template is built upon'
   end
 
-  newproperty(:provision_script, array_matching: :all) do
+  newparam(:provision_script, array_matching: :all) do
     desc "A script (one line per entry) to execute upon provisioning. This will run after packages, if any, are installed."
   end
 
-  newproperty(:pkglist, array_matching: :all) do
+  newparam(:pkglist, array_matching: :all) do
     desc "A list of packages to be installed"
   end
 end
