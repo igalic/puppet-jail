cfca2ab95f4a21b6c26628c7eaa307516a795a15
Igor Galić <igalic@brainsware.at>
2017-12-07 22:45:43 +0100

    add libiocage provider  for jail
    this is mostly boilerplate copied from jail_template
    now with three providers using the same copy/pasted functions, it's time
    to pull them into their own module!

---
+++ lib/puppet/provider/jail/libiocage.rb
@@ -0,0 +1,123 @@
+Puppet::Type.type(:jail).provide(:libiocage) do
+  desc 'Manage creating of jails using ioc(8)'
+  confine    kernel: :freebsd
+  defaultfor kernel: :freebsd
+
+  # this is used for further confinement
+  commands ioc: '/usr/local/bin/ioc'
+
+  def self.ioc(*args)
+    cmd = ['/usr/local/bin/ioc', args].flatten.compact.join(' ')
+    execute(cmd, override_locale: false, failonfail: true, combine: true)
+  end
+
+  def ioc(*args)
+    self.class.ioc(args)
+  end
+
+  def self.get_ioc_json_array(arg)
+    return nil if arg == '-'
+    return nil if arg == ''
+    return nil if arg.nil?
+    return arg if arg.is_a?(Array)
+    return arg.split(',') if arg.is_a?(String)
+    raise("Unexpected Type for 'arg'")
+  end
+
+  def get_ioc_json_array(arg)
+    self.class.get_ioc_json_array(arg)
+  end
+
+  mk_resource_methods
+
+  def initialize(value = {})
+    super(value)
+    @property_flush = {}
+  end
+
+  def self.prefetch(resources)
+    instances.each do |prov|
+      if (resource = resources[prov.name])
+        resource.provider = prov
+      end
+    end
+  end
+
+  def self.instances
+    jails = JSON.parse(ioc('list', '--output-format=json --output=name,release,from_template'))
+    jails.map do |r|
+      fstab = ioc('fstab', 'show', r['name'])
+      fstabs = fstab.split("\n").map do |l|
+        next if l =~ %r{^$|^#}
+        src, dst, type, opts, _dump, _pass, trash = l.split(%r{\s+})
+        raise ArgumentError, "this fstab line cannot be parsed.. in ruby: `#{l}`" unless trash.nil?
+        rw = !(opts =~ %r{\brw\b}).nil?
+
+        fs = { src: src, dst: dst, type: type, rw: rw }
+        # apparently, munge is not ran after self.instances,
+        # so we have to do repeat this:
+        fs.delete(:type) if fs[:type] == 'nullfs'
+        fs.delete(:dst) if fs[:dst] =~ %r{#{fs[:src]}$}
+        fs.delete(:rw) if [:false, 'false', false].include? fs[:rw]
+        fs
+      end.compact
+
+      new(
+        ensure: :present,
+        name: r['name'],
+        release: r['release'],
+        fstab: fstabs
+      )
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def create
+    ip4_addr = ip6_addr = nil
+    ip4_addr = "ip4_addr='#{resource[:ip4_addr]}'" if resource[:ip4_addr]
+    ip6_addr = "ip6_addr='#{resource[:ip6_addr]}'" if resource[:ip6_addr]
+
+    ioc('create', '--release', resource[:release], '--name', resource[:name], ip4_addr, ip6_addr)
+    ioc('set', 'ip4_addr=', 'ip6_addr=', resource[:name])
+    ioc('stop', resource[:name])
+  end
+
+  def release=(value)
+    @property_flush[:release] = value
+  end
+
+  def fstab=(value)
+    @property_flush[:fstab] = value
+  end
+
+  def destroy
+    ioc('destroy', '--force', resource[:name])
+  end
+
+  def flush
+    # the only way to update release, pkglist, or postscript is to recreate
+    if @property_flush[:release] || @property_flush[:pkglist] || @property_flush[:postscript]
+      destroy
+      create
+    end
+
+    if @property_flush[:fstab]
+      desired_fstab = Array(resource[:fstab] == :absent ? [] : resource[:fstab])
+      current_fstab = Array(fstab == :absent ? [] : fstab)
+      (current_fstab - desired_fstab).each do |f|
+        rw = '-rw' if ['true', :true, true].include? f[:rw]
+        ioc('fstab', 'rm', rw, f[:src], resource[:name])
+      end
+
+      (desired_fstab - current_fstab).each do |f|
+        rw = nil
+        rw = '-rw' if ['true', :true, true].include? f[:rw]
+        ioc('fstab', 'add', rw, f[:src], f[:dst], resource[:name])
+      end
+    end
+    @property_flush = resource.to_hash
+  end
+end
