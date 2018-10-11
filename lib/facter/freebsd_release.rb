7e649bbcaeea4bb29e80bd0fdf86f847d0d95751
Igor Galić <igalic@brainsware.at>
2018-10-11 11:40:59 +0200

    add nil check for various parts of the OS fact we use to build our own freebsd_release fact

--- lib/facter/freebsd_release.rb
+++ lib/facter/freebsd_release.rb
@@ -1,6 +1,8 @@
-Facter.add(:freebsd_release) do
-  confine kernel: :freebsd
-  setcode do
-    Facter.value(:os)['release']['full'].gsub(%r{-p\d+$}, '')
+if Facter.value(:os) && Facter.value(:os)['release'] && Facter.value(:os)['release']['full']
+  Facter.add(:freebsd_release) do
+    confine kernel: :freebsd
+    setcode do
+      Facter.value(:os)['release']['full'].gsub(%r{-p\d+$}, '')
+    end
   end
 end
